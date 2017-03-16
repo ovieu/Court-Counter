0d66a3e16631c088cfc8f949d2fca5b1a809986b
ovieu <ovie.u.umukoro@gmail.com>
2017-03-16 05:40:38 -0400

    Fix team B buttons

--- app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         displayForTeamA(scoreTeamA);
     }
 
-    public void addTwForTeamA(View v) {
+    public void addTwoForTeamA(View v) {
         scoreTeamA += 2;
         displayForTeamA(scoreTeamA);
     }
@@ -36,4 +37,30 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
     }
+
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamB(View v) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View v) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View v) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+
 }
